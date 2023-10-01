a757ab0e2b716dd693015a79645dbab19116c656
Pretham Muthappa <98420696+PrethamMuthappa@users.noreply.github.com>
2023-10-02 02:19:35 +0530

    Update main.rs
    added command buffer

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,16 @@ use vulkano::instance::{Instance, InstanceCreateInfo};
 use vulkano::device::QueueFlags;
 use vulkano::device::{Device,DeviceCreateInfo,QueueCreateInfo};
 use vulkano::device::physical::PhysicalDevice;
-use vulkano::memory::allocator::StandardMemoryAllocator;
+use vulkano::memory::allocator::{MemoryAllocatePreference, StandardMemoryAllocator};
 use vulkano::memory::allocator::{AllocationCreateInfo,MemoryUsage};
 use vulkano::buffer::{Buffer,BufferCreateInfo,BufferUsage};
-use core::default::Default;
+use vulkano::command_buffer::allocator::{StandardCommandBufferAlloc, StandardCommandBufferAllocator, StandardCommandBufferAllocatorCreateInfo};
+use vulkano::command_buffer::{AutoCommandBufferBuilder,CommandBufferUsage,CopyBufferInfo};
 use vulkano::swapchain::FullScreenExclusive::Default as OtherDefault;
-
+use vulkano::sync::{self,GpuFuture};
+use vulkano::buffer::BufferContents;
+use core::default::Default;
+use std::error::Error;
 
 
 fn main() {
@@ -53,21 +57,69 @@ let physicaldevice=instance.enumerate_physical_devices().expect("device not crea
     //cretae a memory alloc using stand memory alloc lib
     let memory_allocator=StandardMemoryAllocator::new_default(device.clone());
 
-    let data:i32=12;
-    let buffer=Buffer::from_data(
+
+   let source_content:Vec<i32> = (0..64).collect();
+
+    let source=Buffer::from_iter(
         &memory_allocator,
         BufferCreateInfo {
-            usage:BufferUsage::UNIFORM_BUFFER,
+            usage:BufferUsage::TRANSFER_SRC,
             ..Default::default()
         },
         AllocationCreateInfo {
             usage:MemoryUsage::Upload,
             ..Default::default()
         },
-        data,
+        source_content,
     )
-        .expect("failed to create buffer");
+        .unwrap();
 
+    let dest_content:Vec<i32>=(0..64).map(|_| 0).collect();
+    let destination=Buffer::from_iter(
+        &memory_allocator,
+        BufferCreateInfo {
+          usage:BufferUsage::TRANSFER_DST,
+            ..Default::default()
+        },
+        AllocationCreateInfo {
+            usage:MemoryUsage::Download,
+            ..Default::default()
+        },
+        dest_content,
+    )
+        .unwrap();
+
+    //creatig a new command buffer, but for than we cant use our normal allocator
+
+     let commandbufferalloc=StandardCommandBufferAllocator::new(device.clone(),StandardCommandBufferAllocatorCreateInfo::default(),);
+
+     let mut builder=AutoCommandBufferBuilder::primary(
+         &commandbufferalloc,
+         queue_family_index,
+         CommandBufferUsage::OneTimeSubmit,
+     )
+         .unwrap();
+    builder
+        .copy_buffer(CopyBufferInfo::buffers(source.clone(),destination.clone()))
+        .unwrap();
+
+    let commandbuffer=builder.build().unwrap();
+
+    let future=sync::now(device.clone())
+        .then_execute(queue.clone(),commandbuffer)
+        .unwrap()
+        .then_signal_fence_and_flush()
+        .unwrap();
+
+    future.wait(None).unwrap();
+
+
+
+    let srccontent=source.read().unwrap();
+    let destinationcontent=destination.read().unwrap();
+    assert_eq!(&*srccontent,&*destinationcontent);
+
+    println!("everything succeeded");
 
 
 }
