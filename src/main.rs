a4d516f7f152e9b1dd1be7cbf4e541c359bdcc8c
Pretham Muthappa <98420696+PrethamMuthappa@users.noreply.github.com>
2023-10-02 16:43:08 +0530

    final outcome

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,19 @@ use vulkano::device::{Device,DeviceCreateInfo,QueueCreateInfo};
 use vulkano::device::physical::PhysicalDevice;
 use vulkano::memory::allocator::{MemoryAllocatePreference, StandardMemoryAllocator};
 use vulkano::memory::allocator::{AllocationCreateInfo,MemoryUsage};
+use vulkano::image::{ImageDimensions,StorageImage};
+use vulkano::format::{ClearColorValue, Format};
 use vulkano::buffer::{Buffer,BufferCreateInfo,BufferUsage};
 use vulkano::command_buffer::allocator::{StandardCommandBufferAlloc, StandardCommandBufferAllocator, StandardCommandBufferAllocatorCreateInfo};
-use vulkano::command_buffer::{AutoCommandBufferBuilder,CommandBufferUsage,CopyBufferInfo};
+use vulkano::command_buffer::{AutoCommandBufferBuilder, ClearColorImageInfo, CommandBufferUsage, CopyBufferInfo};
+use vulkano::command_buffer::CopyImageToBufferInfo;
 use vulkano::swapchain::FullScreenExclusive::Default as OtherDefault;
 use vulkano::sync::{self,GpuFuture};
 use vulkano::buffer::BufferContents;
 use core::default::Default;
 use std::error::Error;
+use image::{ImageBuffer,Rgba};
+
 
 
 fn main() {
@@ -58,53 +63,58 @@ let physicaldevice=instance.enumerate_physical_devices().expect("device not crea
     let memory_allocator=StandardMemoryAllocator::new_default(device.clone());
 
 
-   let source_content:Vec<i32> = (0..64).collect();
-
-    let source=Buffer::from_iter(
-        &memory_allocator,
-        BufferCreateInfo {
-            usage:BufferUsage::TRANSFER_SRC,
-            ..Default::default()
-        },
-        AllocationCreateInfo {
-            usage:MemoryUsage::Upload,
-            ..Default::default()
-        },
-        source_content,
-    )
-        .unwrap();
-
-    let dest_content:Vec<i32>=(0..64).map(|_| 0).collect();
-    let destination=Buffer::from_iter(
-        &memory_allocator,
-        BufferCreateInfo {
-          usage:BufferUsage::TRANSFER_DST,
-            ..Default::default()
-        },
-        AllocationCreateInfo {
-            usage:MemoryUsage::Download,
-            ..Default::default()
-        },
-        dest_content,
-    )
-        .unwrap();
-
     //creatig a new command buffer, but for than we cant use our normal allocator
 
      let commandbufferalloc=StandardCommandBufferAllocator::new(device.clone(),StandardCommandBufferAllocatorCreateInfo::default(),);
 
-     let mut builder=AutoCommandBufferBuilder::primary(
-         &commandbufferalloc,
-         queue_family_index,
-         CommandBufferUsage::OneTimeSubmit,
-     )
-         .unwrap();
-    builder
-        .copy_buffer(CopyBufferInfo::buffers(source.clone(),destination.clone()))
+
+
+       let image =StorageImage::new(
+           &memory_allocator,
+           ImageDimensions::Dim2d {
+               width:1024,
+               height:1024,
+               array_layers:1,
+           },
+           Format::R8G8B8A8_UNORM,
+           Some(queue.queue_family_index()),
+
+       )
+           .unwrap();
+
+       let buf=Buffer::from_iter(
+           &memory_allocator,
+           BufferCreateInfo{
+               usage:BufferUsage::TRANSFER_DST,
+               ..Default::default()
+           },
+           AllocationCreateInfo {
+               usage:MemoryUsage::Download,
+               ..Default::default()
+           },
+           (0..1024 * 1024 * 4).map(|_| 0u8),
+
+       )
+           .expect("failed");
+
+    let mut builder=AutoCommandBufferBuilder::primary(
+        &commandbufferalloc,
+        queue.queue_family_index(),
+        CommandBufferUsage::OneTimeSubmit,
+    )
         .unwrap();
 
-    let commandbuffer=builder.build().unwrap();
-
+    builder.clear_color_image(ClearColorImageInfo {
+        clear_value:ClearColorValue::Float([0.0,0.0,1.0,1.0]),
+        ..ClearColorImageInfo::image(image.clone())
+    })
+        .unwrap()
+        .copy_image_to_buffer(CopyImageToBufferInfo::image_buffer(
+            image.clone(),
+            buf.clone(),
+        ))
+        .unwrap();
+let commandbuffer=builder.build().unwrap();
     let future=sync::now(device.clone())
         .then_execute(queue.clone(),commandbuffer)
         .unwrap()
@@ -113,13 +123,9 @@ let physicaldevice=instance.enumerate_physical_devices().expect("device not crea
 
     future.wait(None).unwrap();
 
+    let buffercontent=buf.read().unwrap();
+    let image = ImageBuffer::<Rgba<u8>, _>::from_raw(1024, 1024, &buffercontent[..]).unwrap();
+    image.save("image.png").unwrap();
 
-
-    let srccontent=source.read().unwrap();
-    let destinationcontent=destination.read().unwrap();
-    assert_eq!(&*srccontent,&*destinationcontent);
-
-    println!("everything succeeded");
-
-
+    println!("everything is done");
 }
